1286df51c5b36e78bcf222cba64d56d5feb189b5
Henrique Pereira <henriquepermartins@gmail.com>
2024-12-19 10:48:50 -0300

    feat: table & category filter

--- app/(dashboard)/transacoes/_components/TransactionTable.tsx
+++ app/(dashboard)/transacoes/_components/TransactionTable.tsx
@@ -2,21 +2,107 @@
 
 import { DateToUTCDate } from "@/lib/helpers";
 import { useQuery } from "@tanstack/react-query";
-import React from "react";
-import { ColumnDef } from "@tanstack/react-table";
+import React, { useMemo, useState } from "react";
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  getSortedRowModel,
+  SortingState,
+  useReactTable,
+} from "@tanstack/react-table";
+import { cn } from "@/lib/utils";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { GetTransactionsHistoryResponseType } from "@/app/api/transactions-history/route";
+import SkeletonWrapper from "@/components/SkeletonWrapper";
+import { DataTableColumnHeader } from "@/components/data-table/ColumnHeader";
+import { DataTableFacetedFilter } from "@/components/data-table/FacetedFilters";
 
 interface Props {
   from: Date;
   to: Date;
 }
 
+const emptyData: unknown[] = [];
 type TransactionHistoryRow = GetTransactionsHistoryResponseType[0];
 
-export const columns: ColumnDef<TransactionHistoryRow>[] = [];
+export const columns: ColumnDef<TransactionHistoryRow>[] = [
+  {
+    accessorKey: "category",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Categoria" />
+    ),
+    cell: ({ row }) => (
+      <div className="flex gap-2 capitalize">
+        {row.original.categoryIcon}
+        <div className="capitalize">{row.original.category}</div>
+      </div>
+    ),
+  },
+  {
+    accessorKey: "description",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Descrição" />
+    ),
+    cell: ({ row }) => (
+      <div className="capitalize">{row.original.description}</div>
+    ),
+  },
+  {
+    accessorKey: "type",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Tipo" />
+    ),
+    cell: ({ row }) => (
+      <div
+        className={cn(
+          "capitalize rounded-lg text-center p-2",
+          row.original.type === "income" &&
+            "bg-emerald-400/10 text-emerald-500",
+          row.original.type === "expense" && "bg-red-400/10 text-red-500"
+        )}
+      >
+        {row.original.type}
+      </div>
+    ),
+  },
+  {
+    accessorKey: "amount",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Quantidade" />
+    ),
+    cell: ({ row }) => (
+      <p className="text-md rounded-lg bg-gray-400/5 p-2 text-center font-medium">
+        {row.original.formattedAmount}
+      </p>
+    ),
+  },
+  {
+    accessorKey: "date",
+    header: "Data",
+    cell: ({ row }) => {
+      const date = new Date(row.original.date);
+      const formattedDate = date.toLocaleDateString("default", {
+        timeZone: "UTC",
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+      });
+      return <div className="text-muted-foreground">{formattedDate}</div>;
+    },
+  },
+];
 
 function TransactionTable({ from, to }: Props) {
-  const history = useQuery({
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const history = useQuery<GetTransactionsHistoryResponseType>({
     queryKey: ["transactions", "history", from, to],
     queryFn: () =>
       fetch(
@@ -25,7 +111,97 @@ function TransactionTable({ from, to }: Props) {
         )}&to=${DateToUTCDate(to)}`
       ).then((res) => res.json()),
   });
-  return <div>TransactionTable</div>;
+
+  const table = useReactTable({
+    data: history.data ?? emptyData,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    state: {
+      sorting,
+    },
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+  });
+
+  const categoriesOptions = useMemo(() => {
+    const categoriesMap = new Map();
+    history.data?.forEach((transaction) => {
+      categoriesMap.set(transaction.category, {
+        value: transaction.category,
+        label: `${transaction.categoryIcon} ${transaction.category}`,
+      });
+    });
+    const uniqueCategories = new Set(categoriesMap.values());
+    return Array.from(uniqueCategories);
+  }, [history.data]);
+
+  return (
+    <div className="w-full">
+      <div className="flex flex-wrap items-end justify-between gap-2 py-4">
+        <div className="flex gap-2">
+          {table.getColumn("category") && (
+            <DataTableFacetedFilter
+              title="Categorias"
+              column={table.getColumn("category")}
+              options={categoriesOptions}
+            />
+          )}
+        </div>
+      </div>
+      <SkeletonWrapper isLoading={history.isFetching}>
+        <div className="rounded-md border">
+          <Table>
+            <TableHeader>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <TableRow key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => {
+                    return (
+                      <TableHead key={header.id}>
+                        {header.isPlaceholder
+                          ? null
+                          : flexRender(
+                              header.column.columnDef.header,
+                              header.getContext()
+                            )}
+                      </TableHead>
+                    );
+                  })}
+                </TableRow>
+              ))}
+            </TableHeader>
+            <TableBody>
+              {table.getRowModel().rows?.length ? (
+                table.getRowModel().rows.map((row) => (
+                  <TableRow
+                    key={row.id}
+                    data-state={row.getIsSelected() && "selected"}
+                  >
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell
+                    colSpan={columns.length}
+                    className="h-24 text-center"
+                  >
+                    No results.
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </div>
+      </SkeletonWrapper>
+    </div>
+  );
 }
 
 export default TransactionTable;
